fb81b6546c3f875f6524c18f067ba5e0bf528904
Tom Brewer <tombrewer@Toms-MacBook-Pro.local>
2025-05-09 19:02:37 -0400

    Desktop Registration for Photino

---
+++ Mythetech.Components.Desktop/Photino/PhotinoInteropFileSaveService.cs
@@ -0,0 +1,33 @@
+using Mythetech.Components.Infrastructure;
+
+namespace Mythetech.Components.Desktop.Photino;
+
+public class PhotinoInteropFileSaveService : IFileSaveService
+{
+    private readonly IPhotinoAppProvider _provider;
+
+    public PhotinoInteropFileSaveService(IPhotinoAppProvider provider)
+    {
+        _provider = provider;
+    }
+    
+    public async Task<bool> SaveFileAsync(string fileName, string data)
+    {
+        string? location = await PromptFileSaveAsync(fileName);
+
+        if (string.IsNullOrWhiteSpace(location))
+            return false;
+        
+        await File.WriteAllTextAsync(location, data);
+        return true;
+    }
+
+    public async Task<string?> PromptFileSaveAsync(string fileName)
+    {
+        var app = _provider.Instance;
+
+        string? location = await app.MainWindow.ShowSaveFileAsync("Save File", null, [(fileName, ["sql"])]);
+        
+        return string.IsNullOrEmpty(location) ? default : location;
+    }
+}
--- Mythetech.Components/Infrastructure/IFileSaveService.cs
+++ Mythetech.Components/Infrastructure/IFileSaveService.cs
@@ -1,8 +1,20 @@
 namespace Mythetech.Components.Infrastructure;
 
+/// <summary>
+/// An abstract way to save files for components
+/// </summary>
 public interface IFileSaveService
 {
+    /// <summary>
+    /// Saves a file
+    /// </summary>
+    /// <param name="fileName">Name of the File</param>
+    /// <param name="data">Contents of the file</param>
     public Task<bool> SaveFileAsync(string fileName, string data);
     
+    /// <summary>
+    /// Prompts behavior to save a file
+    /// </summary>
+    /// <param name="fileName">Name of the file</param>
     public Task<string?> PromptFileSaveAsync(string fileName);
 }
