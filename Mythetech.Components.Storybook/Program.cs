15a83e4991d448b14a0595773bc2e4986343fefc
Tom Brewer <tombrewer@Toms-MacBook-Pro.local>
2025-05-16 11:05:08 -0400

    Improve MessageBus Documentation  - Document public interface  - Create simple story book example with markdown to demonstrate usage  - Fix button not having OnClick property to trigger actions

--- Mythetech.Components.Storybook/Program.cs
+++ Mythetech.Components.Storybook/Program.cs
@@ -1,7 +1,9 @@
 ﻿using Mythetech.Components.Storybook;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor;
 using MudBlazor.Services;
+using Mythetech.Components.Infrastructure.MessageBus;
 using Mythetech.Components.WebAssembly;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -10,11 +12,22 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services.AddMemoryCache();
+
 builder.Services.AddMudServices(config =>
 {
     config.PopoverOptions.ThrowOnDuplicateProvider = false;
 });
 
+builder.Services.AddMudMarkdownServices();
+
+
 builder.Services.AddLinkOpeningService();
 
-await builder.Build().RunAsync();
+builder.Services.AddMessageBus();
+
+var host = builder.Build();
+
+host.Services.UseMessageBus();
+    
+await host.RunAsync();
--- Mythetech.Components/Infrastructure/MessageBus/IMessageBus.cs
+++ Mythetech.Components/Infrastructure/MessageBus/IMessageBus.cs
@@ -1,11 +1,27 @@
 namespace Mythetech.Components.Infrastructure.MessageBus;
 
+/// <summary>
+/// An abstraction over a way to send messages from a component or service to other components and services.
+/// </summary>
 public interface IMessageBus
 {
+    /// <summary>
+    /// Publishes a strongly typed message for other components and services.
+    /// </summary>
     Task PublishAsync<TMessage>(TMessage message) where TMessage : class;
     
+    /// <summary>
+    /// Registers a concrete consumer for a given message type to the bus pipeline
+    /// </summary>
     void RegisterConsumerType<TMessage, TConsumer>() where TMessage : class where TConsumer : IConsumer<TMessage>;
     
+    /// <summary>
+    /// Subscribes an IConsumer<TMessage> to the bus.
+    /// </summary>
     void Subscribe<TMessage>(IConsumer<TMessage> consumer) where TMessage : class;
+    
+    /// <summary>
+    /// Removes an IConsumer<TMessage> subscription from the bus.
+    /// </summary>
     void Unsubscribe<TMessage>(IConsumer<TMessage> consumer) where TMessage : class;
 }
