2922a86f0398de615478e0ae75462ba3dcfc881c
Tom Brewer <tombrewer0@gmail.com>
2026-04-08 21:34:59 -0600

    delete redundant dialog command

--- Mythetech.Framework/Components/Dialogs/Commands/ShowDialog.cs
+++
@@ -1,9 +0,0 @@
-using MudBlazor;
-
-namespace Mythetech.Framework.Components.Dialogs.Commands;
-
-/// <summary>
-/// Command to show a dialog via the MessageBus
-/// </summary>
-public record ShowDialog(Type Dialog, string Title, DialogOptions? Options = default, DialogParameters? Parameters = default);
-
